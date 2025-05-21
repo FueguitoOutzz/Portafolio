99cabd9dfe6309f2e86493770d2883b50ccc12e0
FueguitoOutzz <renatochavezz10@gmail.com>
2025-05-20 22:30:14 -0400

    fix v.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    base: '/Portafolio/',
+	site: 'https://github.com/FueguitoOutzz/Portafolio.git',
+	integrations: [mdx(), sitemap()],
 });
